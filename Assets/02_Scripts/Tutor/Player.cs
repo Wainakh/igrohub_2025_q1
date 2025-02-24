660b3a364d6806eaacc531b1e4226cf5410d4fe5
S.Salyukov <serzhik1990mail@gmail.com>
2025-02-24 23:42:12 +0300

    Add movable character

---
+++ Assets/02_Scripts/Tutor/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Igrohub
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+
+        private void Update()
+        {
+            var x = Input.GetAxis("Horizontal");
+            var y = Input.GetAxis("Vertical");
+            if (Math.Abs(x) > float.Epsilon || Math.Abs(y) > float.Epsilon)
+            {
+                var direction = new Vector3(x, 0, y);
+                var movement = Vector3.ClampMagnitude(direction, 1);
+                movement *= _speed;
+                movement *= Time.deltaTime;
+                transform.Translate(movement);
+            }
+        }
+    }
+}
