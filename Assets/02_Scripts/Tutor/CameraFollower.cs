577b2ca781e57369b8acbc75da0c2dca1b9a196f
S.Salyukov <serzhik1990mail@gmail.com>
2025-02-24 23:44:57 +0300

    Add camera follower

---
+++ Assets/02_Scripts/Tutor/CameraFollower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Igrohub
+{
+    public class CameraFollower : MonoBehaviour
+    {
+        [SerializeField] Transform _followTarget;
+        
+        private Vector3 _offset;
+
+        private void Start()
+        {
+            _offset = transform.position - _followTarget.position;
+        }
+
+        private void Update()
+        {
+            if (_followTarget != default)
+                transform.position = _followTarget.position + _offset;
+        }
+    }
+}
